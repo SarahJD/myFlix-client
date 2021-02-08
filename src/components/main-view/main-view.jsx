50c55dc00afeff870f8b45e1e3ad460923216d9e
SarahJD <sarahjdudek@gmail.com>
2021-02-08 16:28:01 +0100

    add routing

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import axios from 'axios';
 import { Row, Col, Button } from 'react-bootstrap';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { LoginView } from '../login-view/login-view';
-import { RegistrationView } from '../registration-view/registration-view'
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import './main-view.scss'; 
@@ -16,10 +16,25 @@ export class MainView extends React.Component {
     super();
 
     this.state = {
-      movies: null,
-      selectedMovie: null, 
+      movies: [],
       user: null
     };
+  }
+
+  // When a user is logged in, the movie list is displayed
+  getMovies(token) {
+    axios.get('https://myflixwomo.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}`} // makes authenticated requests to API
+    })
+    .then(response => {
+      // Assign the result to the state
+      this.setState({
+        movies:response.data
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
   }
 
   componentDidMount() {
@@ -35,55 +50,17 @@ export class MainView extends React.Component {
 
     // When a user successfully logs in, this function updates the user property in state to the particular user
     onLoggedIn(authData) { // authData stands for the user and the token
-    //console.log(authData);
-    this.setState({
-      user: authData.user.Username
-    });
-
-    localStorage.setItem('token', authData.token);
-    localStorage.setItem('user', authData.user.Username);
-    this.getMovies(authData.token); 
-    }
-
-    // When a user is logged in, the movie list is displayed
-    getMovies(token) {
-      axios.get('https://myflixwomo.herokuapp.com/movies', {
-        headers: { Authorization: `Bearer ${token}`} // makes authenticated requests to API
-      })
-      .then(response => {
-        // Assign the result to the state
-        this.setState({
-          movies:response.data
-        });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-    }
-
-    // When a movie is clicked, this function is invoked and updates the state of the 'selectedMovie' property to that movie
-    onMovieClick(movie) {
+      console.log(authData);
       this.setState({
-        selectedMovie: movie
+        user: authData.user.Username
       });
-    }
-
-    // When a user clicks the 'Go home' button in the movie view, this function updated the selectedMovie property to null
-    onClickBack() {
-      this.setState({
-        selectedMovie: null
-      });
-    }
-
-    onClickLogOut() {
-      this.setState({
-        accessToken: localStorage.removeItem('token'),
-        user: localStorage.removeItem('user')
-      })
+      localStorage.setItem('token', authData.token);
+      localStorage.setItem('user', authData.user.Username);
+      this.getMovies(authData.token); 
     }
 
   render() {
-    const { movies, selectedMovie, user } = this.state;
+    const { movies, user } = this.state;
 
     // If there is no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView
     if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
@@ -93,33 +70,29 @@ export class MainView extends React.Component {
 
     // If the state of 'selectedMovie' is not null, that selected movie will be returned otherwise, all movies will be returned 
     return (
-      <React.Fragment>
-        <Row className="myFlix-container">
-          <Col md={10}>
-          <h1 className="myFlix">myFlix</h1> 
-          </Col>
-          <Col>
-           <Button variant="dark" type="submit" className="button" onClick={() => this.onClickLogOut()}>
-              Log out
-            </Button>
-          </Col>
+      <Router>
+        <div className="main-view">
+        <Row>
+          <h1 className="myFlix">myFlix</h1>
         </Row>
-        <Row className="main-view justify-content-md-center">
-          
-          {selectedMovie
-            ? (
-                <Col>
-                  <MovieView movie={selectedMovie} onClickBack={() => this.onClickBack()}/>
-                </Col>
-              )
-            : movies.map(movie => (
-                <Col md={4} s={12} key={movie._id}> 
-                  <MovieCard  movie={movie} onClick={movie => this.onMovieClick(movie)}/>
-                </Col>
-              ))
+        <Row>
+          <Route exact path="/" render={() => {
+            if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+            return movies.map(m => <MovieCard key={m._id} movie={m} />)
           }
+          }/>
         </Row>
-      </React.Fragment>
+          <Route path="/register" render={() => <RegistrationView />} />
+          <Route exact path="/movies/:movieId" render={({match}) => <MovieView movie={movies.find(m => m._id === match.params.movieId)}/>}/>
+          <Route exact path="/genres/:name" render={({match}) => { 
+            if (!movies) return <div className="main-view" />;
+            return <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre}/>} }/>
+          <Route exact path="/directors/:name" render={({match}) => { 
+            if (!movies) return <div className="main-view" />;
+            return <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director}/>}
+          } />
+        </div>
+      </Router>
     );
   }
 }
