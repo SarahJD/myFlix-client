1b0f6ef7fd5752ac654b89f66fd42e2c4d2be450
SarahJD <sarahjdudek@gmail.com>
2021-02-09 17:42:35 +0100

    import GenreView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,6 +8,7 @@ import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { GenreView } from '../genre-view/genre-view';
 import './main-view.scss'; 
 
 export class MainView extends React.Component {
