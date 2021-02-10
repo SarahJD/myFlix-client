ac324356428b2902758b54f59c5a97a6cf0ab3d1
SarahJD <sarahjdudek@gmail.com>
2021-02-10 15:01:28 +0100

    import SCSS file and update handle Registration function

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import { Row, Col, Button, Form } from 'react-bootstrap';
+import './registration-view.scss'; 
 
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState(''); // import useState() method with an empty string
@@ -48,7 +50,7 @@ export function RegistrationView(props) {
   };
     
   return (
-    <Form className="form-inside-input" onSubmit={this.onSubmit} noValidate>
+    <Form className="form-inside-input" onSubmit={handleRegistration} noValidate>
       <Form.Row className="justify-content-md-center">
       <Col md={3}>
         <h1 className="mb-4">Registration</h1>
@@ -59,7 +61,7 @@ export function RegistrationView(props) {
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password: </Form.Label>
           <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Create a Password" required />
-          <Form.Text className="text-muted">
+          <Form.Text className="text-muted desc-text">
             No restrictions
           </Form.Text>
         </Form.Group>
@@ -67,14 +69,11 @@ export function RegistrationView(props) {
           <Form.Label>Email address: </Form.Label>
           <Form.Control type="email" name="txtEmail" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter your email address" required/>
         </Form.Group>
-        <Form.Group controlId="">
+        <Form.Group controlId="date">
           <Form.Label>Birthday: </Form.Label>
           <Form.Control type="date" value={birthday} onChange={e => setBirthday(e.target.value)} placeholder="Enter your birthday" />
-          <Form.Text className="text-muted">
-            YYYY-MM-DD
-          </Form.Text>
         </Form.Group>
-        <Button variant="primary" type="submit" onClick={handleRegistration}>
+        <Button className="btn" variant="dark" className="button mt-4 mb-4" type="submit" onClick={handleRegistration}>
           Register
         </Button>
         </Col>
