299b4619b3d8ded571c494371158529894ff99fc
SarahJD <sarahjdudek@gmail.com>
2021-02-10 14:58:59 +0100

    update order of import links

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
-import './movie-card.scss';
 import { Link } from 'react-router-dom';
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
