a13b4f2b615ef25d45251047c887c15c9414117f
SarahJD <sarahjdudek@gmail.com>
2021-02-22 11:44:01 +0100

    create Store

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import Container from 'react-bootstrap/Container'; 
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
-import { MainView } from './components/main-view/main-view'
+import { MainView } from './components/main-view/main-view';
+import moviesApp from './reducers/reducers';
 
 // Import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
+
+// Create Redux Store
+const store = createStore(moviesApp, devToolsEnhancer());
+// when adding redux_store in addition it says"index.jsx:15 Uncaught ReferenceError: redux_store is not defined", but otherwise the redux devtools don't work
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
@@ -18,8 +26,8 @@ class MyFlixApplication extends React.Component {
   }
 }
 
-// Finds the root of your app
+// Find the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the root DOM element
+// Tell React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
