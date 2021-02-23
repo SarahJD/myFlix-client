c967af417ccf2b3db4b0e8e3631604a4ba11a543
SarahJD <sarahjdudek@gmail.com>
2021-02-23 14:46:57 +0100

    add new action SET_USER

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,11 @@
 // declare ACTION TYPES
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_USER = 'SET_USER';
 
 // ACTION CREATORS
 
-// initializes the movies list with movies
+// initialize the movies list with movies
 export function setMovies(value){
   return { 
     type: SET_MOVIES, 
@@ -12,10 +13,17 @@ export function setMovies(value){
   };
 }
 
-// sets the filter to filter the movies list
+// set the filter to filter the movies list
 export function setFilter(value){
   return { 
     type: SET_FILTER, 
     value 
   };
 }
+
+export function setUser(value){
+  return {
+    type: SET_USER,
+    value
+  }
+};
