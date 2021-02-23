176875187e52dd2f6af49f2b4ba1577acfd6c32e
SarahJD <sarahjdudek@gmail.com>
2021-02-23 14:55:57 +0100

    add propTypes and divs for styling

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import './movies-list.scss';
+
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 import { MovieCard } from '../movie-card/movie-card';
+import { propTypes } from 'react-bootstrap/esm/Image';
 
 const mapStateToProps = state => {
   const { visibilityFilter } = state;
@@ -21,10 +24,18 @@ function MoviesList(props) {
     return <div className="main-view" />
 
   return <div className="movies-list">
-    <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+    <VisibilityFilterInput visibilityFilter={visibilityFilter} className="search-bar"/>
+    <div className="class-container">
+    {/* rendering all movies without filtering */}
     {filteredMovies.map(m => <MovieCard key={m._id} movie={m} />)}
+    </div>
   </div>
 }
 
 // mapStateToProps fransforms the store into props that the MoviesList component will use
 export default connect(mapStateToProps) (MoviesList);
+
+MoviesList.propTypes = {
+  movies: propTypes.array,
+  visibilityFilter: propTypes.func
+}
