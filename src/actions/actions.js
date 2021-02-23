fee370e31bcd55004280d0b6b6999918ffa22acd
SarahJD <sarahjdudek@gmail.com>
2021-02-23 19:54:56 +0100

    define login-actions and function 'login'

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,12 @@
+import axios from 'axios';
+
 // declare ACTION TYPES
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_USER = 'SET_USER';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 // ACTION CREATORS
 
@@ -27,3 +32,44 @@ export function setUser(value){
     value
   }
 };
+
+export function request(user) { 
+  return { 
+    type: LOGIN_REQUEST, 
+    user 
+  } 
+}
+
+export function success(user) { 
+  return { 
+    type: LOGIN_SUCCESS, 
+    user 
+  } 
+}
+    
+export function failure(error) { 
+  return { 
+    type: LOGIN_FAILURE, 
+    error 
+  } 
+}
+
+export function login(username, password) {
+  return dispatch => {
+    dispatch(request({ username }));
+    return axios.post('https://myflixwomo.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+    .then(response => {
+      const data = response.data;
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('user', data.user.Username);
+      dispatch(success(data))
+    })
+    .catch(e => {
+      console.log('no such user');
+      dispatch(failure(e.toString()));
+    });
+  };    
+}
