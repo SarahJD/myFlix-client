85bd011eee9fe4410767872545589fdcaa3d1259
SarahJD <sarahjdudek@gmail.com>
2021-02-23 15:00:34 +0100

    create propTypes and update placeholder text

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
 import Form from 'react-bootstrap/Form';
 
 import { setFilter } from '../../actions/actions';
+import './visibility-filter-input.scss'; 
+import { propTypes } from 'react-bootstrap/esm/Image';
 
 function VisibilityFilterInput(props) {
-  return <Form.Control
+  return <Form.Control className="input-filter"
     onChange={e => props.setFilter(e.target.value)}
     value={props.visibilityFilter}
-    placeholder="filter"
+    placeholder="Search for a Movie Title" 
   />;
 }
 
@@ -18,3 +19,9 @@ export default connect(
   null,
   { setFilter }
 ) (VisibilityFilterInput);
+
+VisibilityFilterInput.propTypes = {
+  setFilter: propTypes.func,
+  visibilityFilter: propTypes.func
+}
+
