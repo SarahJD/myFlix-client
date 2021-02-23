6a7cda09e5a5eb63fa1dc94b476c80b26fd0bafd
SarahJD <sarahjdudek@gmail.com>
2021-02-23 14:54:12 +0100

    add div to bring key-values in line

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -7,18 +7,6 @@ import './movie-view.scss';
 
 export class MovieView extends React.Component {
 
-  // Toggle favorite movie
-  toggleFavorite = m => {
-    let newFavorites = [];
-    if (props.favoriteMovies.includes(m)) {
-      newFavorites = props.favoriteMovies.filter((movie) => movie._id !== m._id)
-      this.setState({ favoriteMovies: newFavorites });
-      return 
-    } 
-    this.setState(state => { favoriteMovies: state.favoriteMovies.push(m)})
-  }
-
-
   render() {
     const { movie } = this.props;
     
@@ -30,19 +18,15 @@ export class MovieView extends React.Component {
           <Row>
             <Col>
               <div>
-                { props.favoriteMovies.length && props.favoriteMovies.map(m => <MovieCard key={m._id} movie={m} />)} 
-                  <Button variant="dark" className="button mt-4 mb-4 profile-btn" type="submit" onClick={this.toggleFavorite}>
-                      Remove              
-                  </Button>
-              </div>
-              <div>
                 <h1 className="title">{movie.Title}</h1>
-                <h2 className="subtitles">Director:</h2>
-                <Link to={`/directors/${movie.Director.Name}`}>
-                  <Button className="link" variant="link">{movie.Director.Name}</Button>
-                </Link>
+                <div className="key-value-container">
+                  <h2 className="subtitles">Director:</h2>
+                  <Link to={`/directors/${movie.Director.Name}`}>
+                    <Button className="link" variant="link">{movie.Director.Name}</Button>
+                  </Link>
+                </div>
               </div>
-              <div>
+              <div className="key-value-container">
                 <h2 className="subtitles">Genre:</h2>
                 <Link to={`/genres/${movie.Genre.Name}`}>
                   <Button className="link" variant="link">{movie.Genre.Name}</Button>
