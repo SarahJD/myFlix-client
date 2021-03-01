52fb021006888e47738775a0d24ad8123b3ca3ee
SarahJD <sarahjdudek@gmail.com>
2021-03-01 09:25:28 +0100

    delete comment

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-//import { devToolsEnhancer } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 require("babel-core/register");
