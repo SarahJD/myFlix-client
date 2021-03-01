d81385d7ed29e7a1ad4a473a1cadfee5c6d84efd
SarahJD <sarahjdudek@gmail.com>
2021-03-01 09:22:15 +0100

    include ToggleFavorites component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,21 +3,22 @@ import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import ToggleFavorites from '../toggle-favorites/toggle-favorites';
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
-  
-  // // Toggle favorite movie
-  // toggleFavorite = m => {
-  //   let newFavorites = [];
-  //   if (props.favoriteMovies.includes(m)) {
-  //     newFavorites = props.favoriteMovies.filter((movie) => movie._id !== m._id)
-  //     this.setState({ favoriteMovies: newFavorites });
-  //     return 
-  //   } 
-  //   this.setState(state => { favoriteMovies: state.favoriteMovies.push(m)})
-  // }
-  
+  state = {
+    favoriteMovies: []
+  }
+
+  componentDidMount(){
+    this.setState({ favoriteMovies: JSON.parse(localStorage.getItem('favoriteMovies'))})
+  }
+
+  handleSetFavorites = favoriteMovies => {
+    this.setState({ favoriteMovies })
+  }
+
   render() {
     const { movie } = this.props;
 
@@ -25,19 +26,13 @@ export class MovieCard extends React.Component {
       <Card style={{ width: '16rem' }} className="m-2 moviecard">
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
-         {/* <div>
-                { props.favoriteMovies.length && props.favoriteMovies.map(m => <MovieCard key={m._id} movie={m} />)} 
-                  <Button variant="dark" className="button mt-4 mb-4 profile-btn" type="submit" onClick={this.toggleFavorite}>
-                      Remove              
-                  </Button>
-              </div> */}
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text className="description">{movie.Description.substring(0, 100)}...</Card.Text>
           <div className="card-footer">
             <Link to={`/movies/${movie._id}`} >
               <Button variant="link" className="linktext">Read More</Button>
             </Link>
-            <img src="../../img/star-regular.svg" alt="star"></img>
+            <ToggleFavorites movie={movie} favoriteMovies={this.state.favoriteMovies} handleSetFavorites={favorites => this.handleSetFavorites(favorites)} />
           </div>
         </Card.Body>
       </Card>
