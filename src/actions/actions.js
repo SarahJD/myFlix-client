2655bcf79d77e33ff8ec2366383420d1713623ee
SarahJD <sarahjdudek@gmail.com>
2021-03-03 19:42:32 +0100

    update formatting

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -11,65 +11,53 @@ export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 // ACTION CREATORS
 
 // initialize the movies list with movies
-export const setMovies = (value) => {
-  return { 
-    type: SET_MOVIES, 
-    value 
-  };
-}
+export const setMovies = (value) => ({
+  type: SET_MOVIES,
+  value,
+});
 
 // set the filter to filter the movies list
-export const setFilter = (value) => {
-  return { 
-    type: SET_FILTER, 
-    value 
-  };
-}
+export const setFilter = (value) => ({
+  type: SET_FILTER,
+  value,
+});
 
-export const setUser = (value) => {
-  return {
-    type: SET_USER,
-    value
-  }
-};
+export const setUser = (value) => ({
+  type: SET_USER,
+  value,
+});
 
-export const request = (user) => { 
-  return { 
-    type: LOGIN_REQUEST, 
-    user 
-  } 
-}
+export const request = (user) => ({
+  type: LOGIN_REQUEST,
+  user,
+});
 
-export const success = (user) => { 
-  return { 
-    type: LOGIN_SUCCESS, 
-    user 
-  } 
-}
-    
-export const failure = (error) => { 
-  return { 
-    type: LOGIN_FAILURE, 
-    error 
-  } 
-}
+export const success = (user) => ({
+  type: LOGIN_SUCCESS,
+  user,
+});
 
-export const login = (username, password) => dispatch => {
+export const failure = (error) => ({
+  type: LOGIN_FAILURE,
+  error,
+});
+
+export const login = (username, password) => (dispatch) => {
   dispatch(request({ username }));
   return axios.post('https://myflixwomo.herokuapp.com/login', {
     Username: username,
-    Password: password
+    Password: password,
   })
-  .then(response => {
-    console.log(response);
-    const data = response.data;
-    localStorage.setItem('token', data.token);
-    localStorage.setItem('user', data.user.Username);
-    localStorage.setItem('favoriteMovies', JSON.stringify(data.user.FavoriteMovies));
-    dispatch(success(data))
-  })
-  .catch(e => {
-    console.log('no such user');
-    dispatch(failure(e.toString()));
-  });
+    .then((response) => {
+      console.log(response);
+      const { data } = response;
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('user', data.user.Username);
+      localStorage.setItem('favoriteMovies', JSON.stringify(data.user.FavoriteMovies));
+      dispatch(success(data));
+    })
+    .catch((e) => {
+      console.log('no such user');
+      dispatch(failure(e.toString()));
+    });
 };
