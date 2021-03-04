893ed111e48dc00e82b02eee025bbea021f48585
SarahJD <sarahjdudek@gmail.com>
2021-03-04 17:28:25 +0100

    update formatting

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -4,52 +4,52 @@ import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
   switch (action.type) {
-    case SET_FILTER:
-      return action.value;
-    default:
-      return state;
+  case SET_FILTER:
+    return action.value;
+  default:
+    return state;
   }
 }
 
 function movies(state = [], action) {
   switch (action.type) {
-    case SET_MOVIES:
-      return action.value;
-    default:
-      return state;
+  case SET_MOVIES:
+    return action.value;
+  default:
+    return state;
   }
 }
 
 function user(state = '', action) {
   switch (action.type) {
-    case SET_USER:
-      return action.value;
-      default:
-        return state;
+  case SET_USER:
+    return action.value;
+  default:
+    return state;
   }
 }
 
-let storedUser = localStorage.getItem('user');
+const storedUser = localStorage.getItem('user');
 const initialAuthState = storedUser ? { loggedIn: true, user: storedUser } : {};
 
 export function authentication(state = initialAuthState, action) {
   switch (action.type) {
-    case 'LOGIN_REQUEST':
-      return {
-        loggingIn: true,
-        user: action.user
-      };
-    case 'LOGIN_SUCCESS':
-      return {
-        loggedIn: true,
-        user: action.user
-      };
-    case 'LOGIN_FAILURE':
-      return {};
+  case 'LOGIN_REQUEST':
+    return {
+      loggingIn: true,
+      user: action.user,
+    };
+  case 'LOGIN_SUCCESS':
+    return {
+      loggedIn: true,
+      user: action.user,
+    };
+  case 'LOGIN_FAILURE':
+    return {};
     // case userConstants.LOGOUT:
     //   return {};
-    default:
-      return state
+  default:
+    return state;
   }
 }
 
@@ -57,8 +57,8 @@ export function authentication(state = initialAuthState, action) {
 const moviesApp = combineReducers({
   visibilityFilter,
   movies,
-  user, 
-  authentication
+  user,
+  authentication,
 });
 
 export default moviesApp;
