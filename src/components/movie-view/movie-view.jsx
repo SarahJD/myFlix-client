2c1a966760b71ca64693f7acdc802b8967781801
SarahJD <sarahjdudek@gmail.com>
2021-03-04 17:20:21 +0100

    update toggle feature for favorite movies

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,50 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import {
+  Button, Col, Container, Row,
+} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+
+import ToggleFavorites from '../toggle-favorites/toggle-favorites';
 import './movie-view.scss';
- 
 
 export class MovieView extends React.Component {
+  state = {
+    favoriteMovies: [],
+  }
+
+  componentDidMount() {
+    this.setState({ favoriteMovies: JSON.parse(localStorage.getItem('favoriteMovies')) });
+  }
+
+  handleSetFavorites = (favoriteMovies) => {
+    this.setState({ favoriteMovies });
+  }
 
   render() {
     const { movie } = this.props;
-    
+
     if (!movie) return null;
 
-    return ( 
+    return (
       <React.Fragment>
-        <Container className="container">
-          <Row>
-            <Col>
-              <div>
+        <div className="movie-poster-container">
+          <div>
+            <div>
+              <div className="title-star-container">
                 <h1 className="title">{movie.Title}</h1>
+                <ToggleFavorites movie={movie} favoriteMovies={this.state.favoriteMovies} handleSetFavorites={(favorites) => this.handleSetFavorites(favorites)} />
+                </div>
                 <div className="key-value-container">
                   <h2 className="subtitles">Director:</h2>
-                  <Link to={`/directors/${movie.Director.Name}`}>
-                    <Button className="link" variant="link">{movie.Director.Name}</Button>
-                  </Link>
+                    <Link to={`/directors/${movie.Director.Name}`}>
+                      <Button className="link" variant="link">{movie.Director.Name}</Button>
+                    </Link>
                 </div>
               </div>
               <div className="key-value-container">
                 <h2 className="subtitles">Genre:</h2>
                 <Link to={`/genres/${movie.Genre.Name}`}>
                   <Button className="link" variant="link">{movie.Genre.Name}</Button>
-                </Link>  
+                </Link>
               </div>
               <div className="description">
-                <h2 className="subtitles">Description: </h2 >
+                <h2 className="subtitles">Description: </h2>
                 <p className="description-text">{movie.Description}</p>
               </div>
-              <Link to="/" >
+              <Link to="/movieslist">
                 <Button variant="dark" className="button mt-4 mb-4" type="submit">Go back</Button>
               </Link>
-            </Col>
-            <Col>
+            </div>
+            <div>
               <img className="movie-poster mt-4 mb-4" src={movie.ImagePath} />
-            </Col>
-          </Row>  
-        </Container>
+            </div>
+          </div>
       </React.Fragment>
 
     );
@@ -52,13 +67,13 @@ export class MovieView extends React.Component {
 }
 
 MovieView.propTypes = {
-movie: PropTypes.shape({
-  Title: PropTypes.string.isRequired,
-  Description: PropTypes.string.isRequired,
-  ImagePath: PropTypes.string.isRequired, 
-  Genre: PropTypes.shape({
-    Name: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired
-  })
-}).isRequired
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
 };
