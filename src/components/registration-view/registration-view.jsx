33e91aec99103169a45938f256b3a45436019e3c
SarahJD <sarahjdudek@gmail.com>
2021-03-04 17:25:11 +0100

    update formatting

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Col, Button, Form } from 'react-bootstrap';
-import './registration-view.scss'; 
+import './registration-view.scss';
 
 export function RegistrationView() {
-  const [ username, setUsername ] = useState(''); // import useState() method with an empty string
-  const [ password, setPassword ] = useState(''); 
-  const [ confirmPassword, setConfirmPassword ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ birthday, setBirthday ] = useState('');
+  const [username, setUsername] = useState(''); // import useState() method with an empty string
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
 
   const [usernameErr, setUsernameErr] = useState({});
   const [emailErr, setEmailErr] = useState({});
@@ -22,19 +22,19 @@ export function RegistrationView() {
       Username: username,
       Password: password,
       Email: email,
-      Birthday: birthday
+      Birthday: birthday,
     })
-    .then(response => {
-      const data = response.data;
-      console.log(data);
-      window.open('/', '_self'); // '_self' in order to open the page in the current tab and redirect to login ('/')
-    })
-    .catch(e => {
-      console.log('error registering the user');
-      formValidation();
-    });
+      .then((response) => {
+        const { data } = response;
+        console.log(data);
+        window.open('/', '_self'); // '_self' in order to open the page in the current tab and redirect to login ('/')
+      })
+      .catch((e) => {
+        console.log('error registering the user');
+        formValidation();
+      });
   };
-    
+
   const formValidation = () => {
     const usernameErr = {};
     const passwordErr = {};
@@ -42,7 +42,7 @@ export function RegistrationView() {
     const emailErr = {};
     const birthdayErr = {};
     const emailReg = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-    let checkEmail = emailReg.test(email);
+    const checkEmail = emailReg.test(email);
     let isValid = true;
 
     if (birthday === '') {
@@ -120,47 +120,41 @@ export function RegistrationView() {
           <h1 className="mb-4">Registration</h1>
           <Form.Group controlId="">
             <Form.Label>Username: </Form.Label>
-            <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Create a Username" />
+            <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Create a Username" />
             <Form.Text className="text-muted desc-text">
               5-10 Characters
             </Form.Text>
-            {Object.keys(usernameErr).map((key) => {
-            return <div style={{ color: 'black' }}>{usernameErr[key]}</div>;
-          })}
+            {Object.keys(usernameErr).map((key) => <div style={{ color: 'black' }}>{usernameErr[key]}</div>)}
           </Form.Group>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address: </Form.Label>
-            <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter your email address" />
-            {Object.keys(emailErr).map((key) => {
-            return <div style={{ color: 'black' }}>{emailErr[key]}</div>;
-          })}
+            <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email address" />
+            {Object.keys(emailErr).map((key) => <div style={{ color: 'black' }}>{emailErr[key]}</div>)}
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password: </Form.Label>
-            <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Create a Password" />
+            <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Create a Password" />
             <Form.Text className="text-muted desc-text">
               5-10 Characters
             </Form.Text>
-            {Object.keys(passwordErr).map((key) => {
-            return <div style={{ color: 'black' }}>{passwordErr[key]}</div>;
-          })}
+            {Object.keys(passwordErr).map((key) => <div style={{ color: 'black' }}>{passwordErr[key]}</div>)}
           </Form.Group>
-          <Form.Group controlId='formBasicConfirmPassword'>
+          <Form.Group controlId="formBasicConfirmPassword">
             <Form.Label>Confirm Password: </Form.Label>
-            <Form.Control type='password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder='Enter Password'
+            <Form.Control
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder="Enter Password"
             />
-            {Object.keys(confirmPasswordErr).map((key) => {
-              return (
-                <div style={{ color: 'black' }}>{confirmPasswordErr[key]}</div>
-              );
-            })}
+            {Object.keys(confirmPasswordErr).map((key) => (
+              <div style={{ color: 'black' }}>{confirmPasswordErr[key]}</div>
+            ))}
           </Form.Group>
           <Form.Group controlId="date">
             <Form.Label>Birthday: </Form.Label>
-            <Form.Control type='date' value={birthday} onChange={e => setBirthday(e.target.value)} placeholder="Enter Your Birthday" />
-            {Object.keys(birthdayErr).map((key) => {
-            return <div style={{ color: 'black' }}>{birthdayErr[key]}</div>;
-          })}
+            <Form.Control type="date" value={birthday} onChange={(e) => setBirthday(e.target.value)} placeholder="Enter Your Birthday" />
+            {Object.keys(birthdayErr).map((key) => <div style={{ color: 'black' }}>{birthdayErr[key]}</div>)}
           </Form.Group>
           <Button className="btn" variant="dark" className="button mt-4 mb-4" type="submit" onClick={handleRegistration}>
             Register
@@ -168,6 +162,5 @@ export function RegistrationView() {
         </Col>
       </Form.Row>
     </Form>
-  )
+  );
 }
-
