f0e9df489c19eea65c2bee01ad8b61c3433d23de
SarahJD <sarahjdudek@gmail.com>
2021-03-04 17:25:51 +0100

    add media query

--- src/components/toggle-favorites/toggle-favorites.jsx
+++ src/components/toggle-favorites/toggle-favorites.jsx
@@ -2,33 +2,32 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import starRegular from '../../img/star-regular.svg';
 import starSolid from '../../img/star-solid.svg';
-//import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-import './toggle-favorites.scss'; 
+import './toggle-favorites.scss';
 
 export default function ToggleFavorites(props) {
-
-  const toggleMovie = async m => {
-    let favoriteMovies = props.favoriteMovies;
-    let accessToken = localStorage.getItem('token');
-    let presentFavorite = favoriteMovies.includes(m);
+  const toggleMovie = async (m) => {
+    const { favoriteMovies } = props;
+    const accessToken = localStorage.getItem('token');
+    const presentFavorite = favoriteMovies.includes(m);
     let newFavorites = [];
     if (presentFavorite && (accessToken !== null)) {
-      //await axios.put(`https://myflixwomo.herokuapp.com/users/${user}/Movies/${m._id}`, { headers: { Authorization: `Bearer ${token}`}});
-      newFavorites = favoriteMovies.filter((movie) => movie._id !== m._id)
-      props.handleSetFavorites(newFavorites)
+      // await axios.put(`https://myflixwomo.herokuapp.com/users/${user}/Movies/${m._id}`, { headers: { Authorization: `Bearer ${token}`}});
+      newFavorites = favoriteMovies.filter((movie) => movie._id !== m._id);
+      props.handleSetFavorites(newFavorites);
     }
     if (!presentFavorite && (accessToken !== null)) {
-      //await axios.post(`https://myflixwomo.herokuapp.com/users/${user}/Movies/${m._id}`, { headers: { Authorization: `Bearer ${token}`}});
+      // await axios.post(`https://myflixwomo.herokuapp.com/users/${user}/Movies/${m._id}`, { headers: { Authorization: `Bearer ${token}`}});
       newFavorites.push(m);
       props.handleSetFavorites(newFavorites);
-     }
-    //localStorage.setItem('favoriteMovies', JSON.stringify(FavoriteMovies));
-  }
+    }
+    // localStorage.setItem('favoriteMovies', JSON.stringify(FavoriteMovies));
+  };
 
   return (
     <div onClick={() => toggleMovie(props.movie)}>
-      {<img id="star" src={props.favoriteMovies.includes(props.movie) ? starSolid : starRegular} alt="star"></img>}
+      <img id="star" src={props.favoriteMovies.includes(props.movie) ? starSolid : starRegular} alt="star" />
     </div>
   );
 }
