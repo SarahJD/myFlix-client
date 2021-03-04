9173363b716cc796d9a08585083fa745f8523c37
SarahJD <sarahjdudek@gmail.com>
2021-03-04 17:17:11 +0100

    update formatting and change conditions definer to accessToken

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import { Row, Col, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Link, Switch,
+} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 // import actions
@@ -16,81 +18,96 @@ import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
 import { ProfileView } from '../profile-view/profile-view';
 
-import './main-view.scss'; 
+import './main-view.scss';
 
 class MainView extends React.Component {
   state = {
-    accessToken: localStorage.getItem('token')
+    accessToken: '',
   };
 
   componentDidMount() {
     // when page loads check if the user is logged in
-    let accessToken = localStorage.getItem('token');
+    const accessToken = localStorage.getItem('token');
     if (accessToken !== null) {
       // let user = localStorage.getItem('user');
       // this.props.setUser(user);
+      this.setState({ accessToken });
       this.getMovies(accessToken);
     }
   }
 
   // When a user is logged in, the movie list is displayed
-  getMovies = async(token) => {
+  getMovies = async (token) => {
     axios.get('https://myflixwomo.herokuapp.com/movies', {
-      headers: { Authorization: `Bearer ${token}`} // makes authenticated requests to API
+      headers: { Authorization: `Bearer ${token}` }, // makes authenticated requests to API
     })
-    .then(response => {
+      .then((response) => {
       // Assign the result Redux
-      this.props.setMovies(response.data); 
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+        this.props.setMovies(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   handleLogOut = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
-    window.open("/", "_self")
+    window.open('/', '_self');
   }
-    
+
   render() {
-    const { movies, user } = this.props;
-    
+    const { movies, authentication } = this.props;
+
     return (
       <Router>
         <div className="main-view">
 
-          {user && (
-            <Row className="main-row">
-                <Link to="/movielist" className="myFlix" >
-                  myFlix
-                </Link> 
-                <div>
-                  <Link to="/profile">
-                    <Button className="main-buttons" variant="dark">Profile</Button> 
-                  </Link>
-                  <Button className="main-buttons" variant="dark" type="submit" onClick={this.handleLogOut}>Log Out</Button> 
-                </div>
+          {this.state.accessToken && (
+            <Row className="main-row main-heading-row">
+              <Link to="/movieslist" className="myFlix">
+                myFlix
+              </Link>
+              <div>
+                <Link to="/profile">
+                  <Button className="main-buttons" variant="dark">Profile</Button>
+                </Link>
+                <Button className="main-buttons" variant="dark" type="submit" onClick={this.handleLogOut}>Log Out</Button>
+              </div>
             </Row>
           )}
 
           <Switch>
-            <Route exact path="/" render={() => <LoginView setToken ={token => this.setState({accessToken: token})} loginUser = {(name, pass) => this.props.login(name, pass)} />} />
-            <Route exact path="/movielist" render={() => {
-              if (!this.state.accessToken) return <LoginView setToken ={token => this.setState({accessToken: token})} loginUser = {(name, pass) => this.props.login(name, pass)} />;
-              {/* Home is the default route */}
-              return <MoviesList movies={movies}/>
-            }} />
+            <Route exact path="/" render={() => <LoginView setToken={(token) => this.setState({ accessToken: token })} loginUser={(name, pass) => this.props.login(name, pass)} />} />
+            <Route
+              exact
+              path="/movieslist"
+              render={() => {
+                if (!this.state.accessToken) return <LoginView setToken={(token) => this.setState({ accessToken: token })} loginUser={(name, pass) => this.props.login(name, pass)} />;
+                { /* Home is the default route */ }
+                return <MoviesList movies={movies} />;
+              }}
+            />
             <Route exact path="/register" render={() => <RegistrationView />} />
             <Route exact path="/profile" render={() => <ProfileView user={this.props.user} movies={this.props.movies} />} />
-            <Route exact path="/movies/:movieId" render={({match}) => <MovieView movie={movies.find(m => m._id === match.params.movieId)}/>}/>
+            <Route exact path="/movies/:movieId" render={({ match }) => <MovieView movie={movies.find((m) => m._id === match.params.movieId)} />} />
             {/* Even if the movie object is loaded later, show the route */}
-            <Route exact path="/genres/:name" render={({match}) => { 
-              if (!movies) return <div className="main-view" />;
-              return <GenreView movies={this.props.movies} genre={ (movies.length > 0) ? movies.find(m => m.Genre.Name === match.params.name).Genre : {} }/>} }/>
-            <Route exact path="/directors/:name" render={({match}) => { 
-              if (!movies) return <div className="main-view" />;
-              return <DirectorView movies={this.props.movies} director={ (movies.length > 0) ? movies.find(m => m.Director.Name === match.params.name).Director : {} }/>} }/>
+            <Route
+              exact
+              path="/genres/:name"
+              render={({ match }) => {
+                if (!movies) return <div className="main-view" />;
+                return <GenreView movies={this.props.movies} genre={(movies.length > 0) ? movies.find((m) => m.Genre.Name === match.params.name).Genre : {}} />;
+              }}
+            />
+            <Route
+              exact
+              path="/directors/:name"
+              render={({ match }) => {
+                if (!movies) return <div className="main-view" />;
+                return <DirectorView movies={this.props.movies} director={(movies.length > 0) ? movies.find((m) => m.Director.Name === match.params.name).Director : {}} />;
+              }}
+            />
           </Switch>
         </div>
       </Router>
@@ -98,16 +115,14 @@ class MainView extends React.Component {
   }
 }
 
-let mapStateToProps = state => {
-  return { movies: state.movies, user: state.user }
-}
+const mapStateToProps = (state) => ({ movies: state.movies, user: state.user });
 
-export default connect(mapStateToProps, { setMovies, setUser, login }) (MainView);
+export default connect(mapStateToProps, { setMovies, setUser, login })(MainView);
 
 MainView.proptypes = {
   movies: PropTypes.array,
   user: PropTypes.string,
   setMovies: PropTypes.func,
   setUser: PropTypes.func,
-  login: PropTypes.func
-}
+  login: PropTypes.func,
+};
